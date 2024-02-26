e30a1fa23e9ccba08bc23d1eac0927dd13c796e7
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-25 19:13:01 -0600

    fixed a big bug

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 // Require controller modules.
 const book_controller = require("../controllers/bookController");
-console.log(book_controller.index, 'this is controller');
 const author_controller = require("../controllers/authorController");
 const genre_controller = require("../controllers/genreController");
 const book_instance_controller = require("../controllers/bookinstanceController");
